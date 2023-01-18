039382061c36ea0fef29aab7f1f991c240b3ae0d
Marco Travaglini <marctravaglini@gmail.com>
2023-01-18 13:12:04 +0100

    Refactor tasks.js

--- Tasks.js
+++ Tasks.js
@@ -15,7 +15,7 @@ import {
 // import { useNavigation } from '@react-navigation/native';
 import { FontAwesome } from '@expo/vector-icons';
 import { db } from './firebase.config';
-import { doc, collection, query, getDoc, setDoc, onSnapshot, orderBy } from "firebase/firestore";
+import { doc, collection, query, getDoc, setDoc, deleteDoc, onSnapshot, orderBy } from "firebase/firestore";
 
 // use custom style sheet
 const styles = require('./Style.js');
@@ -23,18 +23,17 @@ const styles = require('./Style.js');
 export function TasksScreen({ route, navigation }) {
 
     const [user, setUser] = useState('');
-    const [email, setEmail] = useState(route.params.email);
+    // const [email, setEmail] = useState(route.params.email);
+    const email = route.params.email;
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState('');
     const [isLoading, setLoading] = useState(true);
 
     const userRef = doc(db, "users", route.params.email);
     const tasksRef = query(collection(db, "users", route.params.email, "tasks"), orderBy('createdAt'));
-    const taskRef = db.collection("users/" + route.params.email + "/tasks");
-    
 
+    // get user 
     useEffect(() => {
-        // get user from database
         async function getUser() {
             try {
                 const docSnap = await getDoc(userRef);
@@ -46,8 +45,8 @@ export function TasksScreen({ route, navigation }) {
         getUser();
     }, [])
 
+    // get tasks
     useEffect(() => {
-        // get tasks from database
         var unsubscribe;
         var taskObj;
         async function getTasks() {
@@ -57,19 +56,7 @@ export function TasksScreen({ route, navigation }) {
                     querySnapshot.forEach((doc) => {
                         taskObj = doc.data();
                         taskObj.taskTitle = doc.id;
-                        // const taskTitle = doc.id;
-                        // const createdAt = new Date(doc.data().createdAt);
-                        // const startDate = new Date(doc.data().startDate);
-                        // const endDate = new Date(doc.data().endDate);
                         retrievedTasks.push(taskObj
-                        //     {
-                        //     // id: doc.id,
-                        //     assignee: doc.data().assignee,
-                        //     taskTitle: doc.id,
-                        //     createdAt: new Date(doc.data().createdAt),
-                        //     startDate: new Date(doc.data().startDate),
-                        //     endDate: new Date(doc.data().endDate)
-                        // }
                         )
                     })
                     setTasks(retrievedTasks)
@@ -83,119 +70,19 @@ export function TasksScreen({ route, navigation }) {
         getTasks();
         return function cleanup() {
             unsubscribe();
-          };
+        };
     }, [])
 
-    // useEffect(() => {
-    //     // get tasks from database
-    //     async function getTasks() {
-    //         try {
-    //             const tasksSnap = await getDocs(tasksRef);
-    //             const retrievedTasks = []
-
-    //             tasksSnap.forEach((doc) => {
-    //                 // doc.data() is never undefined for query doc snapshots
-    //                 const taskTitle = doc.id; // check this
-    //                 const taskDate = new Date(doc.data().createdAt);
-    //                 retrievedTasks.push({
-    //                     // id: doc.id,
-    //                     taskTitle: taskTitle,
-    //                     taskDate: taskDate
-    //                 })
-    //             });
-    //             setTasks(retrievedTasks)
-    //             setLoading(false);
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     }
-    //     getTasks();
-    // }, [])
-
-
-    // useEffect(() => {
-    //     tasksRef
-    //         .orderBy('createdAt', 'desc')
-    //         .onSnapshot(
-    //             querySnapshot => {
-    //                 const tasks = []
-    //                 querySnapshot.forEach((doc) => {
-    //                     // const taskTitle = doc.data(); // check this
-    //                     const taskTitle = doc.id; // check this
-    //                     const taskDate = new Date(doc.data().createdAt);
-    //                     tasks.push({
-    //                         // id: doc.id,
-    //                         taskTitle: taskTitle,
-    //                         taskDate: taskDate
-    //                     })
-    //                 })
-    //                 setTasks(tasks)
-    //             }
-    //         )
-
-    //     setLoading(false);
-
-    // }, [])
-
     // delete a task
-
     const deleteTask = async (taskTitle) => {
         try {
-            taskRef
-                .doc(taskTitle)
-                .delete()
+            await deleteDoc(doc(db, "users", route.params.email, "tasks", taskTitle));
         } catch (error) {
             alert(error);
         }
     }
 
     // add a task
-
-    // const addTask = () => {
-    //     // check we have one to add
-    //     if (newTask && newTask.length > 0) {
-    //         const timestamp = Math.floor(Date.now()) //serverTimestamp();
-    //         const data = {
-    //             uid: uid,
-    //             title: newTask,
-    //             createdAt: timestamp
-    //         }
-    //         tasksRef
-    //             .add(data)
-    //             .then(() => {
-    //                 setNewTask('');
-    //                 Keyboard.dismiss();
-    //                 // success message
-    //                 // alert("Added!");
-    //             })
-    //             .catch(error => {
-    //                 alert(error);
-    //             })
-    //     }
-    // }
-
-    // const addTask = () => {
-    //     // check we have one to add
-    //     if (newTask && newTask.length > 0) {
-    //         const timestamp = Math.floor(Date.now()) //serverTimestamp();
-    //         const data = {
-    //             createdAt: timestamp
-    //         }
-    //         setDoc(doc(db, "users", email, "tasks", newTask), data)
-    //             .then(() => {
-    //                 setNewTask('');
-    //                 Keyboard.dismiss();
-    //                 // success message
-    //                 // alert("Added!");
-    //             })
-    //             .catch(error => {
-    //                 alert(error);
-    //             })
-    //     }
-    // }
-
-
-
     const addTask = async () => {
         // const addTask = () => {
         // check we have one to add
@@ -208,7 +95,6 @@ export function TasksScreen({ route, navigation }) {
                     endDate: timestamp + (24 * 60 * 60 * 1000),
                     createdAt: timestamp
                 }
-                // setDoc(doc(taskRef, tasks.taskTitle), data)
                 setDoc(doc(db, "users", email, "tasks", newTask), data)
                 setNewTask('');
             } catch (error) {
@@ -216,8 +102,6 @@ export function TasksScreen({ route, navigation }) {
             }
         }
     }
-
-
 
     return (
         <SafeAreaView style={styles.safeView}>
@@ -268,7 +152,7 @@ export function TasksScreen({ route, navigation }) {
                                     <View>
                                         <Pressable
                                             style={styles.listContainer}
-                                            onPress={() => navigation.navigate('TaskDetail', {item})}
+                                            onPress={() => navigation.navigate('TaskDetail', { item })}
                                         >
                                             <FontAwesome
                                                 style={styles.listDelIcon}
