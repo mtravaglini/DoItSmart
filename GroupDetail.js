e3250e2d4afaecd081dd1a70e6d67ecd4477f170
Marco Travaglini <marctravaglini@gmail.com>
2023-02-05 17:54:22 +0100

    rename groupText style to groupResourceText

--- GroupDetail.js
+++ GroupDetail.js
@@ -316,7 +316,7 @@ export function GroupDetailScreen({ route, navigation }) {
                       <Pressable key={item.uid}
                         onPress={() => confirmDeleteGroupMembership(item.uid, item.name)}
                       >
-                        <Text style={styles.groupText}>
+                        <Text style={styles.groupResourceText}>
                           {item.name}
                         </Text>
                       </Pressable>
@@ -328,7 +328,7 @@ export function GroupDetailScreen({ route, navigation }) {
                       setBackgroundOpacity(.33)
                     }}
                   >
-                    <Text style={styles.groupText}>
+                    <Text style={styles.groupResourceText}>
                       +
                     </Text>
                   </Pressable>
